95b84cd5d3a9dadf777df75e687aed16fdcbe681
ShahadatPOM <16103112@iubat.edu>
2021-06-09 11:39:23 +0600

    pulling

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,11 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
-    public function categoryProducts($categoryId){
-        return view('shop');
+    public function categoryProducts($categoryId=null){
+
+        if(empty($categoryId)){
+
+            $products = Product::with('sku', 'images')->get();
+        }
+
+        if(!empty($categoryId)){
+        $products = Product::with('sku', 'images')->where('categoryId', $categoryId)->get();
+        dd($products);
+        }
+
+        return view('shop', compact('products'));
     }
 }
