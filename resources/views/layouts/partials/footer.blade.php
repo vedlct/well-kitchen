35d3f7bf0ee9a6ab487c71d7b6bb03bd70176151
saikat chandra <saikotchondrobd@gmail.com>
2021-06-25 19:59:31 +0600

    contact, my-account from submit done

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -48,10 +48,10 @@
                         </div>
                         <div class="footer-list">
                             <ul>
-                                <li><a href="">Facebook</a></li>
-                                <li><a href="#">Twitter</a></li>
-                                <li><a href="#">Instagram</a></li>
-                                <li><a href="#">Youtube</a></li>
+                                <li><a href="{{$setting->facebook}}">Facebook</a></li>
+                                <li><a href="{{$setting->twitter}}">Twitter</a></li>
+                                <li><a href="{{$setting->instagram}}">Instagram</a></li>
+                                {{-- <li><a href="#">Youtube</a></li> --}}
                             </ul>
                         </div>
                     </div>
